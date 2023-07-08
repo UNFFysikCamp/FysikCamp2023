bb821862ea611b5170da34d8cd250e700ad05483
Mikkel <MikkelMM99@gmail.com>
2023-07-08 22:08:10 +0200

    Test less logic

--- bash_script/save_to_drive.sh
+++ bash_script/save_to_drive.sh
@@ -5,7 +5,7 @@ cd /content/FysikCamp2023
 source_folder="/content/FysikCamp2023"
 
 # Folder to exclude from copying
-excluded_folders=("bash_script" ".git")
+excluded_folders="bash_script" 
 
 
 excluded_files=(".gitignore" "README.md" "THIS.ipynb")
@@ -13,14 +13,14 @@ excluded_files=(".gitignore" "README.md" "THIS.ipynb")
 #Change the year here
 destination_folder="/content/drive/MyDrive/Colab Notebooks/UNF_Fysik_Camp2023"
 # Copy folders and contents excluding the specified folder and files, without overwriting existing files
-rsync -av --exclude-from=<(printf "%s\n" "${excluded_folders[@]/#/--exclude=}") --exclude-from=<(printf "%s\n" "${excluded_files[@]/#/--exclude=}") --ignore-existing "$source_folder/" "$destination_folder/"
+rsync -av --exclude="$excluded_folder" --exclude=".git" --exclude-from=<(printf "%s\n" "${excluded_files[@]}") --ignore-existing "$source_folder/" "$destination_folder/"
 
 echo "Finished creating main folder"
 excluded_files=(".gitignore" "README.md")
 #Change the year here
 destination_folder="/content/drive/MyDrive/Colab Notebooks/UNF_Fysik_Camp2023/source"
 # Copy folders and contents excluding the specified folder and files
-rsync -av --exclude-from=<(printf "%s\n" "${excluded_folders[@]/#/--exclude=}") --exclude-from=<(printf "%s\n" "${excluded_files[@]/#/--exclude=}") "$source_folder/" "$destination_folder/"
+rsync -av --exclude="$excluded_folder" --exclude=".git" --exclude-from=<(printf "%s\n" "${excluded_files[@]/#/--exclude=}") "$source_folder/" "$destination_folder/"
 
 echo "Finished creating source folder"
 
