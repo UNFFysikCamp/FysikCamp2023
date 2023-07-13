5bee5732dac670e317b71dfe989ca4864b2e6965
Mikkel <MikkelMM99@gmail.com>
2023-07-13 17:48:56 +0200

    test 3

--- GEO/fitmodel.py
+++ GEO/fitmodel.py
@@ -1,13 +1,13 @@
 def fitmodel(path):
     import numpy as np
     import os
-    
+
     # slmatlab
     os.rename(path+"SLMATLAB.M", path+"slmatlab.m_bad")
     os.rename(path+"slmatlab.m_bad", path+"slmatlab.m")
 
-    slmatlab_file_path = 'slmatlab.m'  # Replace with the actual file path of "slmatlab.m"
-    txt_file_path = 'slmatlab.txt'  # Specify the desired output text file path
+    slmatlab_file_path = path+'slmatlab.m'  # Replace with the actual file path of "slmatlab.m"
+    txt_file_path = path+'slmatlab.txt'  # Specify the desired output text file path
 
     with open(slmatlab_file_path, 'r') as slmatlab_file:
         slmatlab_contents = slmatlab_file.read()
@@ -15,7 +15,7 @@ def fitmodel(path):
     with open(txt_file_path, 'w') as txt_file:
         txt_file.write(slmatlab_contents)
 
-    fit = np.genfromtxt('slmatlab.txt',skip_header=2,skip_footer=1)
+    fit = np.genfromtxt(path+'slmatlab.txt',skip_header=2,skip_footer=1)
     return fit
 
     
